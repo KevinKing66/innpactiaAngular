b6d7e1f11b6ae12d75bd0b661398410dd901551c
KevinKing66 <kevinvallenato2002@gmail.com>
2022-01-21 21:35:00 -0500

    master

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,10 +13,12 @@ export class HomeComponent implements OnInit {
   user: any = JSON.parse(this.x);
   city: string = "";
   info: any = "";
-  queries: any = [] ;
-  paginas: any = []
+  queries: any = [];
   findB: boolean = false;
   ItemsPP: number = 5;
+  filtre: string = "";
+  filtreA: boolean = false;
+  filtreL: any =  []
   p: number = 1;
   constructor(private service: WeatherService, private serviceL: LoginService) {
 
@@ -69,4 +71,22 @@ export class HomeComponent implements OnInit {
   find(){
     this.findB  = this.findB ? false : true;
   }
+
+  filtreF(x: string){
+    let i= 0;
+    this.filtreL =  []
+    while(i < this.user.queries.length && x.length > 1){
+      if(this.user.queries[i].name.toLowerCase().includes(x)){
+        this.filtreL.push(this.user.queries[i].name)
+        console.log(this.filtreL)
+      };
+      if(this.user.queries[i].name.toLowerCase() == this.filtre.toLowerCase()){
+          this.filtreL.includes(this.user.queries[i]) ? console.log("ejecucion completa") : this.filtreL.push(this.user.queries[i]);
+      }
+      i++;
+    }
+    
+  }
+
+  
 }
